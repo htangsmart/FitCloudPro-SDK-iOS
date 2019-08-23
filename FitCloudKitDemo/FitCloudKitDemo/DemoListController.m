25e26d90fbc850cfcb4cd2b71dd4a6bb58d5dd62
pcjbird <pcjbird@hotmail.com>
2019-08-23 23:31:10 +0800

    demo

--- FitCloudKitDemo/FitCloudKitDemo/DemoListController.m
+++ FitCloudKitDemo/FitCloudKitDemo/DemoListController.m
@@ -37,10 +37,13 @@
 
 -(void) fetchSportsDataToday
 {
+    __weak typeof(self) weakSelf = self;
     [FitCloudKit requestHealthAndSportsDataTodayWithBlock:^(BOOL succeed, FitCloudDailyHealthAndSportsDataObject *dataObject, NSError *error) {
         if([dataObject isKindOfClass:[FitCloudDailyHealthAndSportsDataObject class]])
         {
-            XLOG_INFO(@"%@", APP_LOG_STRING(@"\n今日运动数据：\n步数：%@\n距离：%@\n卡路里：%@\n深睡：%@\n浅睡：%@\n平均心率：%@", @(dataObject.steps), @(dataObject.distance), @(dataObject.calory), @(dataObject.deepSleepMinutes),@(dataObject.shallowSleepMinutes),@(dataObject.averageHeartRate)));
+            NSString * log = APP_LOG_STRING(@"\n今日运动数据：\n步数：%@\n距离：%@\n卡路里：%@\n深睡：%@\n浅睡：%@\n平均心率：%@", @(dataObject.steps), @(dataObject.distance), @(dataObject.calory), @(dataObject.deepSleepMinutes),@(dataObject.shallowSleepMinutes),@(dataObject.averageHeartRate));
+            XLOG_INFO(@"%@", log);
+            [weakSelf.view makeToast:NSLocalizedString(@"View the results in the console.", nil)];
         }
         
     }];
@@ -48,9 +51,11 @@
 
 -(void) manualSyncData
 {
+    __weak typeof(self) weakSelf = self;
     [FitCloudKit manualSyncDataWithProgress:^(CGFloat progress, NSString *tip) {
         XLOG_INFO(@"%@", APP_LOG_STRING(@"同步进度：%.0f%%, %@",progress*100.0f, tip));
     } block:^(BOOL succeed, NSArray<FitCloudManualSyncRecordObject*> *records, NSError *error) {
+        [weakSelf.view makeToast:NSLocalizedString(@"View the results in the console.", nil)];
         BOOL hasRecords = [records isKindOfClass:[NSArray class]] && [records count] > 0;
         if(succeed && hasRecords)
         {
