184a2509e675a454c2c52e64a597df492ce876ac
Mr.P <pcjbird@users.noreply.github.com>
2022-09-22 15:40:08 +0800

    FitCloudDFUKit v1.3.0-beta.8

--- FitCloudDFUKit/FitCloudDFUKit.framework/Headers/FitCloudDFUKit.h
+++ FitCloudDFUKit/FitCloudDFUKit.framework/Headers/FitCloudDFUKit.h
@@ -8,7 +8,7 @@
 //  框架名称:FitCloudDFUKit.framework
 //  框架功能:iOS framework for FitCloud Smart Bracelet Device Firmware Upgrade, which is responsible for the upgrade of bracelet firmware. FitCloud智能手环固件升级的iOS框架，负责手环固件升级等功能的封装。
 //  修改记录:
-//     pcjbird    2022-09-21  Version:1.3.0-beta.7 Build:202209210001
+//     pcjbird    2022-08-04  Version:1.3.0-beta.5 Build:202208040002
 //                            1.更新错误码翻译
 //                            2.升级Realtek固件升级程序调用方式
 //                            3.新增越南语
