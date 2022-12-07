f93f48f5c6cf85e98ddae3fba25a57af9eac94d8
Mr.P <pcjbird@users.noreply.github.com>
2022-12-07 21:15:25 +0800

    FitCloudKit v1.2.7-beta.8

--- FitCloudKit/FitCloudKit.framework/Headers/FitCloudKit.h
+++ FitCloudKit/FitCloudKit.framework/Headers/FitCloudKit.h
@@ -9,7 +9,7 @@
 //  框架功能:iOS framework for fitCloud smart watch, which is responsible for the communication with the watch.
 //          FitCloud 智能手表的 iOS 框架，负责与智能手表设备通信等功能的封装。
 //  修改记录:
-//     pcjbird    2022-12-07  Version:1.2.7-beta.7 Build:202212070001
+//     pcjbird    2022-12-07  Version:1.2.7-beta.8 Build:202212070002
 //                            1.新增禁用睡眠模块
 //                            2.新增核酸码推送功能 @see allowNucleicAcidQRCode
 //                            3.天气新增大气压强/风力/能见度信息
