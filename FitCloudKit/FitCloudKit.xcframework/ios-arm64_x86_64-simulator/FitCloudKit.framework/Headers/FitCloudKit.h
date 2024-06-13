d02a2b03bbbfd54485df222af82c70b07c4b0df0
Mr.P <pcjbird@users.noreply.github.com>
2024-06-13 22:43:02 +0800

    FitCloudKit v1.2.9-beta.126

--- FitCloudKit/FitCloudKit.xcframework/ios-arm64_x86_64-simulator/FitCloudKit.framework/Headers/FitCloudKit.h
+++ FitCloudKit/FitCloudKit.xcframework/ios-arm64_x86_64-simulator/FitCloudKit.framework/Headers/FitCloudKit.h
@@ -9,7 +9,7 @@
 //  框架功能:iOS framework for fitCloud smart watch, which is responsible for the communication with the watch.
 //          FitCloud 智能手表的 iOS 框架，负责与智能手表设备通信等功能的封装。
 //  修改记录:
-//     pcjbird    2024-06-13  Version:1.2.9-beta.125 Build:20240613002
+//     pcjbird    2024-06-14  Version:1.2.9-beta.12 Build:20240614001
 //                            1.新增创维光伏数据支持, @see withSkyworthPV
 //                            2.新增一些调试日志
 //                            3.板球比赛数据指令支持, @see withCricketMatch
