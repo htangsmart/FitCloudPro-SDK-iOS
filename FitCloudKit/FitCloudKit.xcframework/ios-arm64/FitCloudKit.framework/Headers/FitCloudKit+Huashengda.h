f71ae1b2550263029294801cb980c5cc7d819ecf
Mr.P <pcjbird@users.noreply.github.com>
2024-06-22 20:50:48 +0800

    FitCloudKit v1.2.9-beta.136

--- FitCloudKit/FitCloudKit.xcframework/ios-arm64/FitCloudKit.framework/Headers/FitCloudKit+Huashengda.h
+++ FitCloudKit/FitCloudKit.xcframework/ios-arm64/FitCloudKit.framework/Headers/FitCloudKit+Huashengda.h
@@ -16,7 +16,20 @@
 /// Query whether Huashengda watch is restore to factory settings
 /// - Parameters:
 ///   - completion: the completion callback
+///
+/// > Warning: Please do not invoke when in automatic connecting process, usually invoke before bind or when receive FITCLOUDEVENT_PERIPHERAL_ALREADYUNBUND_OR_BIND_BY_OTHERCLIENT_NOTIFY event
 +(void) queryHuashengdaWatchWhetherRestoredToFactorySettingsWithCompletion:(void(^_Nullable)(BOOL success, BOOL restored, NSError* _Nullable error))completion;
+
+
+#pragma mark - Query whether Huashengda watch current user id whether match to the user id provided
+
+/// Query whether Huashengda watch current user id whether match to the user id provided
+/// - Parameters:
+///   - userId: the user id you provided for testing
+///   - completion: the completion callback
+///
+/// > Warning: Please do not invoke when in automatic connecting process, usually invoke before bind
++(void) queryHuashengdaWatchCurrentUserIdWetherMatch:(NSString* _Nonnull)userId completion:(void(^_Nullable)(BOOL success, BOOL isMatch, NSError* _Nullable error))completion;
 
 
 @end
