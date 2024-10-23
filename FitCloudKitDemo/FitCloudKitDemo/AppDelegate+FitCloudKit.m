906b7affe7908ced34f2f1c2123e0f2845b65100
Mr.P <pcjbird@users.noreply.github.com>
2024-10-23 21:46:13 +0800

    some demo code for muslim prayer features

--- FitCloudKitDemo/FitCloudKitDemo/AppDelegate+FitCloudKit.m
+++ FitCloudKitDemo/FitCloudKitDemo/AppDelegate+FitCloudKit.m
@@ -81,12 +81,88 @@
     //the App can actively synchronize the movement health data of the bracelet to achieve the function of background synchronization data. Please add it according to the specific project needs. Note that frequent synchronization data in the background may affect the power consumption of the bracelet device.
 }
 
+- (NSArray<FitCloudIslamicEventModel *> *)getIslamicEvents {
+    // 这里可以实现你的节日逻辑，返回包含信息的 IslamicHoliday 对象的数组
+    // 下面的代码仅作示例
+    FitCloudIslamicEventModel *holiday1 = [[FitCloudIslamicEventModel alloc] init];
+    {
+        FitCloudIslamicDateModel* islamicDate = [FitCloudIslamicDateModel new];
+        islamicDate.islamicYear = 1446;
+        islamicDate.islamicMonth = 1;
+        islamicDate.islamicDay = 1;
+        holiday1.islamicDate = islamicDate;
+        holiday1.durationOfEventInDays = 3;
+        holiday1.correspondingGregorianDate = [NSDate date];
+    }
+
+    FitCloudIslamicEventModel *holiday2 = [[FitCloudIslamicEventModel alloc] init];
+    {
+        FitCloudIslamicDateModel* islamicDate = [FitCloudIslamicDateModel new];
+        islamicDate.islamicYear = 1446;
+        islamicDate.islamicMonth = 2;
+        islamicDate.islamicDay = 10;
+        holiday2.islamicDate = islamicDate;
+        holiday2.durationOfEventInDays = 5;
+        holiday2.correspondingGregorianDate = [[NSDate date] dateByAddingTimeInterval:30*24*60*60];
+    }
+    
+    FitCloudIslamicEventModel *holiday3 = [[FitCloudIslamicEventModel alloc] init];
+    {
+        FitCloudIslamicDateModel* islamicDate = [FitCloudIslamicDateModel new];
+        islamicDate.islamicYear = 1446;
+        islamicDate.islamicMonth = 4;
+        islamicDate.islamicDay = 12;
+        holiday3.islamicDate = islamicDate;
+        holiday3.durationOfEventInDays = 8;
+        holiday3.correspondingGregorianDate = [[NSDate date] dateByAddingTimeInterval:3*30*24*60*60];
+    }
+    
+    
+    FitCloudIslamicEventModel *holiday4 = [[FitCloudIslamicEventModel alloc] init];
+    {
+        FitCloudIslamicDateModel* islamicDate = [FitCloudIslamicDateModel new];
+        islamicDate.islamicYear = 1446;
+        islamicDate.islamicMonth = 5;
+        islamicDate.islamicDay = 15;
+        holiday4.islamicDate = islamicDate;
+        holiday4.durationOfEventInDays = 2;
+        holiday4.correspondingGregorianDate = [[NSDate date] dateByAddingTimeInterval:4*30*24*60*60];
+    }
+    
+    FitCloudIslamicEventModel *holiday5 = [[FitCloudIslamicEventModel alloc] init];
+    {
+        FitCloudIslamicDateModel* islamicDate = [FitCloudIslamicDateModel new];
+        islamicDate.islamicYear = 1446;
+        islamicDate.islamicMonth = 7;
+        islamicDate.islamicDay = 18;
+        holiday5.islamicDate = islamicDate;
+        holiday5.durationOfEventInDays = 6;
+        holiday5.correspondingGregorianDate = [[NSDate date] dateByAddingTimeInterval:5*30*24*60*60];
+    }
+    
+    FitCloudIslamicEventModel *holiday6 = [[FitCloudIslamicEventModel alloc] init];
+    {
+        FitCloudIslamicDateModel* islamicDate = [FitCloudIslamicDateModel new];
+        islamicDate.islamicYear = 1446;
+        islamicDate.islamicMonth = 9;
+        islamicDate.islamicDay = 18;
+        holiday6.islamicDate = islamicDate;
+        holiday6.durationOfEventInDays = 9;
+        holiday6.correspondingGregorianDate = [[NSDate date] dateByAddingTimeInterval:8*30*24*60*60];
+    }
+
+    return @[holiday1, holiday2, holiday3, holiday4, holiday5, holiday6];
+}
+
 /// 手表端请求当前回历节日信息
 ///
 /// > The watch side request the current islamic events information
 -(void) OnRequestCurrentIslamicEvents
 {
     XLOG_INFO(@"手表请求回历节日信息...");
+    [FitCloudKit setCurrentIslamicEvents:[self getIslamicEvents] completion:^(BOOL succeed, NSError * _Nonnull error) {
+        
+    }];
 }
 
 /// 手表端请求克尔白数据
