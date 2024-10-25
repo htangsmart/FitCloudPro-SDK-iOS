4f94d697efa4b5519b79a21b43ddffa588b5f004
Mr.P <pcjbird@users.noreply.github.com>
2024-10-25 11:04:50 +0800

    some demo code for muslim prayer features

--- FitCloudKitDemo/FitCloudKitDemo/AppDelegate+FitCloudKit.m
+++ FitCloudKitDemo/FitCloudKitDemo/AppDelegate+FitCloudKit.m
@@ -161,7 +161,7 @@
 {
     XLOG_INFO(@"手表请求回历节日信息...");
     
-    XLOG_INFO(@"回历节日信息:%@", [[self getIslamicEvents] debugDescription]);
+    XLOG_INFO(@"设置回历节日信息:%@", [[self getIslamicEvents] debugDescription]);
     [FitCloudKit setCurrentIslamicEvents:[self getIslamicEvents] completion:^(BOOL succeed, NSError * _Nonnull error) {
         
     }];
@@ -179,7 +179,7 @@
     ka.locationServiceState = APPLOCATIONSERVICESTATE_AVAILABLE;
     ka.azimuthAngle = kaaba.a*/
     
-    XLOG_INFO(@"克尔白信息:%@", [[MuslimPrayerParams instance].kaaba debugDescription]);
+    XLOG_INFO(@"设置克尔白信息:%@", [[MuslimPrayerParams instance].kaaba debugDescription]);
     [FitCloudKit setKaabaData:[MuslimPrayerParams instance].kaaba completion:^(BOOL succeed, NSError * _Nonnull error) {
         
     }];
