810946882483a82e6ed3a5994be501d74c0401fa
Mr.P <pcjbird@users.noreply.github.com>
2025-12-28 21:48:08 +0800

    FitCloudKit v1.3.2-beta.33
    - Added flag to indicate whether the device supports earbuds related APIs.

--- FitCloudKit/FitCloudKit.xcframework/ios-arm64_x86_64-simulator/FitCloudKit.framework/Headers/FitCloudKit.h
+++ FitCloudKit/FitCloudKit.xcframework/ios-arm64_x86_64-simulator/FitCloudKit.framework/Headers/FitCloudKit.h
@@ -10,7 +10,7 @@
 //          FitCloudPro 智能手表 iOS 框架，封装了与手表设备通信等核心功能。
 //
 //  构建版本：
-//      pcjbird    2025-12-28  Version:1.3.2-beta.32 Build:20251228001
+//      pcjbird    2025-12-28  Version:1.3.2-beta.33 Build:20251228002
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
--- FitCloudKit/RELEASE_CN.md
+++ FitCloudKit/RELEASE_CN.md
@@ -1,6 +1,6 @@
 # 版本更新日志
 
-## pcjbird 2025-12-28 Version:1.3.2-beta.32 Build:20251228001
+## pcjbird 2025-12-28 Version:1.3.2-beta.33 Build:20251228002
 
 - 在连接过程中阻止设备扫描。
 - 新增 `FITCLOUDEVENT_PERIPHERAL_SCANSTART_ERROR_NOTIFY` 事件，用于通知设备扫描开始时出现错误。
