0772e3ab365236729946ec23399d9de1fe47760b
Mr.P <pcjbird@users.noreply.github.com>
2026-03-04 13:57:04 +0800

    revise

--- FitCloudWFKit/ABParTool.xcframework/ios-arm64_x86_64-simulator/ABParTool.framework/Headers/ParTool.h
+++ FitCloudWFKit/ABParTool.xcframework/ios-arm64_x86_64-simulator/ABParTool.framework/Headers/ParTool.h
@@ -22,6 +22,25 @@ NS_ASSUME_NONNULL_BEGIN
 /// - Returns: PAR格式图像数据
 + (nullable NSData *)parFromRaw:(NSData *)rawImageData width:(int)width height:(int)height runAlpha:(BOOL)runAlpha useFilter:(BOOL)useFilter supportRotate:(BOOL)supportRotate;
 
+/// 转换原始图像数据到RLE格式图像
+/// - Parameters:
+///   - rawImageData: 原始图像数据
+///   - width: 图像宽度
+///   - height: 图像高度
+///   - transparentColor: 透明色
+/// - Returns: RLE格式图像数据
++ (nullable NSData *)rleFromRaw:(NSData *)rawImageData width:(int)width height:(int)height transparentColor:(int)transparentColor;
+
+/// 转换原始图像数据到PAR V3格式图像
+/// - Parameters:
+///   - rawImageData: 原始图像数据
+///   - width: 图像宽度
+///   - height: 图像高度
+///   - runAlpha: 是否处理Alpha通道
+///   - useFilter: 是否使用滤镜
+/// - Returns: PAR V3格式图像数据
++ (nullable NSData *)parv3FromRaw:(NSData *)rawImageData width:(int)width height:(int)height runAlpha:(BOOL)runAlpha useFilter:(BOOL)useFilter;
+
 /// 转换UIImage到PAR格式图像
 /// - Parameters:
 ///   - image: UIImage
@@ -39,6 +58,36 @@ NS_ASSUME_NONNULL_BEGIN
 ///   - supportRotate: PAR图像是否支持旋转
 /// - Returns: PAR格式图像数据
 + (nullable NSData *)parFromImageData:(NSData *)imageData runAlpha:(BOOL)runAlpha useFilter:(BOOL)useFilter supportRotate:(BOOL)supportRotate;
+
+/// 转换UIImage到PAR V3格式图像
+/// - Parameters:
+///   - image: UIImage
+///   - runAlpha: 是否处理Alpha通道
+///   - useFilter: 是否使用滤镜
+/// - Returns: PAR V3格式图像数据
++ (nullable NSData *)parv3FromImage:(UIImage *)image runAlpha:(BOOL)runAlpha useFilter:(BOOL)useFilter;
+
+/// 转换压缩图像数据到PAR V3格式图像
+/// - Parameters:
+///   - imageData: 压缩图像数据，格式为[UIImage imageWithData:data]支持格式
+///   - runAlpha: 是否处理Alpha通道
+///   - useFilter: 是否使用滤镜
+/// - Returns: PAR V3格式图像数据
++ (nullable NSData *)parv3FromImageData:(NSData *)imageData runAlpha:(BOOL)runAlpha useFilter:(BOOL)useFilter;
+
+/// 转换UIImage到RLE格式图像
+/// - Parameters:
+///   - image: UIImage
+///   - transparentColor: 透明色
+/// - Returns: RLE格式图像数据
++ (nullable NSData *)rleFromImage:(UIImage *)image transparentColor:(int)transparentColor;
+
+/// 转换压缩图像数据到RLE格式图像
+/// - Parameters:
+///   - imageData: 压缩图像数据，格式为[UIImage imageWithData:data]支持格式
+///   - transparentColor: 透明色
+/// - Returns: RLE格式图像数据
++ (nullable NSData *)rleFromImageData:(NSData *)imageData transparentColor:(int)transparentColor;
 
 @end
 
